1328ff8872922b34c94de756eabd3db59f8fde57
khj20231204 <natista00@naver.com>
2024-10-28 22:52:56 +0900

    login design

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,12 +1,17 @@
-const signUpBtn = document.getElementById("signUp");
-const signInBtn = document.getElementById("signIn");
-const container = document.querySelector(".container");
+
 
 window.onload = function() {
+  const signUpBtn = document.getElementById("signUp");
+  const signInBtn = document.getElementById("signIn");
+  const container = document.querySelector(".container");
+
    signUpBtn.addEventListener("click", () => {
+      alert("df");
      container.classList.add("right-panel-active");
+     
    });
    signInBtn.addEventListener("click", () => {
+    alert("geg");
      container.classList.remove("right-panel-active");
    });
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import LoginForm from '../components/Login/LoginForm';
+import LoginFormJs from './../components/Login/LoginFormJs';
 
 const Login = () => {
    return (
       <>
          <Header/>
          <div className='container' style={{marginTop:100}}>
-         <LoginForm/>
+         {/* <LoginForm/> */}
+         <LoginFormJs/>
          </div>
       </>
    );
