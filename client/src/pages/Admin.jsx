f798c2e6347d0dcafb92d9e7149af3d013f84a83
khj20231204 <natista00@naver.com>
2024-11-04 12:20:56 +0900

    주소 수정

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,15 +1,18 @@
 import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header/Header';
-import LoginContextProvider from '../contexts/LoginContextProvider';
+import {LoginContext} from '../contexts/LoginContextProvider';
 import { useNavigate } from 'react-router-dom';
 
 const Admin = () => {
 
-   const { isLogin, roles } = useContext(LoginContextProvider)
+   const { isLogin, roles, userInfo } = useContext(LoginContext)
    const navigate = useNavigate();
 
    //관리자 권한이 있는 사람만 로그인, 아니면 메인으로 이동
    useEffect(() => {
+
+      //if(!userInfo) return;
+
       if(!isLogin){
          alert(`로그인이 필요합니다.`);
          navigate('/login');
