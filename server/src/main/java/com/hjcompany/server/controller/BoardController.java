e9ccd9343692b49eb89a7a8e6933757d49661430
khj20231204 <natista00@naver.com>
2024-11-06 06:55:51 +0900

    403 error2

--- server/src/main/java/com/hjcompany/server/controller/BoardController.java
+++ server/src/main/java/com/hjcompany/server/controller/BoardController.java
@@ -3,8 +3,6 @@ package com.hjcompany.server.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hjcompany.server.service.BoardServiceImpl;
@@ -15,16 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/board")
 public class BoardController {
    
    @Autowired
    private BoardServiceImpl boardServiceImpl;
 
    //@Secured("") 게시판을 보는 것은 모든 권한
-   @GetMapping("/list")
    /* public ResponseEntity<?> list(String pageNum, Board board, Model model) { */
-   public ResponseEntity<?> list() {
+   public ResponseEntity<?> list2() {
         System.out.println("board/list");
        return new ResponseEntity<>(null);
    }
