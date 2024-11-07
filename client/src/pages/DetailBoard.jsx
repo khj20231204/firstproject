1e995569fc4d713b177aa62d99231a71ce04eb73
khj20231204 <natista00@naver.com>
2024-11-07 17:48:35 +0900

    boardlist 완성

--- client/src/pages/DetailBoard.jsx
+++ client/src/pages/DetailBoard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DetailBoardForm from '../components/DetailBoard/DetailBoardForm';
 import Header from '../components/Header/Header';
 
+//DetailBoard에서 현재페이지와 search, keyword를 가져와야 한다.
 const DetailBoard = () => {
    return (
       <>
