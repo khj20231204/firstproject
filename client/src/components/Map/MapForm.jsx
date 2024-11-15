2a79cd62167060b1b7c4290582c440e7cea6f935
khj20231204 <natista00@naver.com>
2024-11-15 10:27:07 +0900

    학원 오전

--- client/src/components/Map/MapForm.jsx
+++ client/src/components/Map/MapForm.jsx
@@ -4,10 +4,36 @@ const {kakao} = window;
 
 const MapForm = () => {
 
-   
+   let [location, setLocation] = useState({
+      Lat : 33.450701,
+      Lng : 126.570667 
+   });
 
+   useEffect(() => {
+      const script = document.createElemenf ("script");
+
+      script.src = "";
+      script.async = true;
+
+      script.onload = () => {
+
+         window.kakao.maps.load = () => {
+            const mapContainer = document.getElementById("map");
+            const mapOption = {
+               center : location,
+               level : 3
+            }
+
+            new window.kakao.maps.Map(mapContainer, mapOption);
+         }
+      }
+
+      document.head.appendChild(script) ;
+
+   })
+   
    return (
-     <div>
+     <div id="map">
 
      </div>
    );
